cd4f9d75e7ad7fae7f77c44373103b893f4b816b
Northman94 <Zhan2000@bigmir.net>
2017-06-17 17:16:16 +0300

    ю

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -25,13 +25,18 @@ namespace Snake
             leftLine.Draw();
             rightLine.Draw();
 
+
+
+
             // Отрисовка точек
 
-            Point p1 = new Point(1, 3, '*');
+            Point p1 = new Point(3, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+
 
             // Создаем линию
             HorizontalLine line = new HorizontalLine(5, 10, 8 , '+');
