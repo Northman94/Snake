c274ac0dee0dbdbdeb00a4936ff277d6216f53bf
Northman94 <Zhan2000@bigmir.net>
2017-06-17 17:11:44 +0300

    8-1

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            
+            // Установить размер окна и убрать возможность пролистывать экран
+            Console.SetBufferSize(120, 40);
+
+            // Отрисовка рамочки
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+            // Отрисовка точек
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
