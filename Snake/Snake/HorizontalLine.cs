8b6c5548c6e083ee0365bcf0826838cd28eb8ec6
Northman94 <Zhan2000@bigmir.net>
2017-06-26 20:48:20 +0300

    v

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -8,21 +8,6 @@ namespace Snake
 {
     class HorizontalLine : Figure
     {
-        #region Before_Bad_Change
-        //public HorizontalLine(int xLeft, int xRight, int y, char sym)
-        //{
-        //    pList = new List<Point>();
-
-        //    for (int x = xLeft; x <= xRight; x++)
-        //    {
-        //        Point p = new Point(x, y, sym);
-        //        pList.Add(p);
-        //    }
-        //}
-
-        //// В 14 уроке все кардинально ппоменяется без толкового объяснения
-        #endregion
-
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
@@ -32,16 +17,5 @@ namespace Snake
                 pList.Add(p);
             }
         }
-
-        #region Отменяем эти изменения обратно
-        //public override void Draw()
-        //{
-        //    Console.ForegroundColor = ConsoleColor.Yellow;
-
-        //    base.Draw();
-
-        //    Console.ForegroundColor = ConsoleColor.White;
-        //}
-#endregion
     }
 }
