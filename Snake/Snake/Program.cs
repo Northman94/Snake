70b3ce75ae58ce7b9c3b8a235eaa81519b6940d1
Northman94 <Zhan2000@bigmir.net>
2017-06-26 20:03:48 +0300

    .

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -66,40 +66,80 @@ namespace Snake
         //            snake.HandleKey(key.Key);
         //        }
         //    }
-
-
         //}
         #endregion
 
-        static void Main(string[] args)
-        {
-            VerticalLine vl = new VerticalLine(0, 10, 5, '%');
-            Draw(vl);
+        #region Strange_Change
+        //static void Main(string[] args)
+        //{
+        //    VerticalLine vl = new VerticalLine(0, 10, 5, '%');
+        //    Draw(vl);
 
-            Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
+        //    Point p = new Point(4, 5, '*');
+        //    Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+        //    Draw(fSnake);
+        //    Snake snake = (Snake)fSnake;
 
-            HorizontalLine hl = new HorizontalLine(0, 5, 6, '&');
+        //    HorizontalLine hl = new HorizontalLine(0, 5, 6, '&');
 
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(vl);
-            figures.Add(hl);
+        //    List<Figure> figures = new List<Figure>();
+        //    figures.Add(fSnake);
+        //    figures.Add(vl);
+        //    figures.Add(hl);
 
-            foreach (var f in figures)
-            {
-                f.Draw();
+        //    foreach (var f in figures)
+        //    {
+        //        f.Draw();
+        //    }
+
+        //    Console.ReadKey();
+        //}
+
+        //static void Draw(Figure figure)
+        //{
+        //    figure.Draw();
+        //}
+        #endregion
+
+        Console.SetWindowSize(80, 25);
+        Console.SetBufferSize( 80, 25 );
+
+			Walls walls = new Walls(80, 25);
+        walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+        Snake snake = new Snake(p, 4, Direction.RIGHT);
+        snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+        Point food = foodCreator.CreateFood();
+        food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail() )
+				{
+					break;
+				}
+				if(snake.Eat(food ) )
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+                Thread.Sleep( 100 );
+
+				if (Console.KeyAvailable )
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key );
+				}
             }
-
-            Console.ReadKey();
-        }
-
-        static void Draw(Figure figure)
-        {
-            figure.Draw();
-        }
 
     }
 }
