3602cd0c3ca24bda8ff0f2a6c57b7dfeb69c504d
Northman94 <Zhan2000@bigmir.net>
2017-06-26 20:22:19 +0300

    .

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -102,27 +102,27 @@ namespace Snake
         #endregion
 
         Console.SetWindowSize(80, 25);
-        Console.SetBufferSize( 80, 25 );
+        Console.SetBufferSize(80, 25);
 
 			Walls walls = new Walls(80, 25);
-        walls.Draw();
+            walls.Draw();
 
 			// Отрисовка точек			
 			Point p = new Point(4, 5, '*');
         Snake snake = new Snake(p, 4, Direction.RIGHT);
-        snake.Draw();
+            snake.Draw();
 
 			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
         Point food = foodCreator.CreateFood();
-        food.Draw();
+             food.Draw();
 
 			while (true)
 			{
-				if (walls.IsHit(snake) || snake.IsHitTail() )
+				if (walls.IsHit(snake) || snake.IsHitTail())
 				{
 					break;
 				}
-				if(snake.Eat(food ) )
+				if(snake.Eat(food))
 				{
 					food = foodCreator.CreateFood();
 					food.Draw();
@@ -132,12 +132,12 @@ namespace Snake
 					snake.Move();
 				}
 
-                Thread.Sleep( 100 );
+                Thread.Sleep(100);
 
-				if (Console.KeyAvailable )
+				if (Console.KeyAvailable)
 				{
 					ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key );
+                    snake.HandleKey(key.Key);
 				}
             }
 
